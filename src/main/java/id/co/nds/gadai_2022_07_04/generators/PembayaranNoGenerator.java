bf13eadac12ac19fd72bc80f86ce338a3324379e
Alvin Wicaksono <alvin.wicaksono@ti.ukdw.ac.id>
2022-08-02 09:23:53 +0700

    Update Generator no Pembayaran

--- src/main/java/id/co/nds/gadai_2022_07_04/generators/PembayaranNoGenerator.java
+++ src/main/java/id/co/nds/gadai_2022_07_04/generators/PembayaranNoGenerator.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.YearMonth;
 
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
@@ -14,12 +15,20 @@ public class PembayaranNoGenerator implements IdentifierGenerator{
     public Serializable generate(SharedSessionContractImplementor ssci, Object o) throws HibernateException {
         Connection connection = ssci.connection();
 
+        int year = Integer.parseInt(Integer.toString(YearMonth.now().getYear()).substring(2));
+
+         int month = YearMonth.now().getMonthValue();
+
+         String y = String.format("%2d", year);
+         String m = String.format("%02d", month);
+
+
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT COUNT(*) AS seq FROM TX_PEMBAYARAN_H");
+            PreparedStatement ps = connection.prepareStatement("SELECT COUNT(*) AS seq FROM TX_PEMBAYARAN_H WHERE no_pembayaran LIKE 'PCT-"+ y + "-" + m + "%' ");
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                int seq = rs.getInt("seq") + 1;
-                String code = String.format("PCT-YYMM%05d", seq);
+                int seq = rs.getInt("seq")+1;
+                String code = String.format("PCT-%2d-%02d-%05d", year, month, seq);
                 System.out.println("Generated Stock Code: " + code);
                 return code;
             } else {
